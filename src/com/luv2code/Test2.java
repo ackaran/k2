378c25f62fa88410fad18e44b925ff2c8a8d597b
karn <karn@LAPTOP-HQCD5CSC>
2021-06-15 22:14:20 +0530

    Hi, I have Entered new Code. please check and verify
    Thank you.

---
+++ src/com/luv2code/Test2.java
@@ -0,0 +1,11 @@
+package com.luv2code;
+
+public class Test2 {
+	public static void main(String[]args) {
+		String Hungry="yes";
+		if(Hungry == "yes") {
+			System.out.println("I am hungry");
+		}
+	}
+
+}
